489132da0df8c1781d9c395dea154226837a367d
mrsirfloppa <125816149+mrsirfloppa@users.noreply.github.com>
2023-04-02 16:16:07 -0600

    updaste

--- pyweb.py
+++ pyweb.py
@@ -1,91 +1,72 @@
 import sys
-from PyQt5.QtCore import QUrl, Qt
-from PyQt5.QtGui import QKeySequence
-from PyQt5.QtWidgets import (QApplication, QLineEdit, QMainWindow, QVBoxLayout, QWidget,
-                             QTabWidget, QPushButton, QHBoxLayout, QShortcut)
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+import tkinter as tk
+from cefpython3 import cefpython as cef
 
-class BrowserTab(QWidget):
-    def __init__(self, parent=None, url=None):
-        super().__init__(parent)
-        layout = QVBoxLayout()
+class MainFrame(tk.Frame):
+    def __init__(self, root):
+        self.browser_frame = None
+        self.navigation_bar = None
 
-        self.browser = QWebEngineView()
-        self.browser.urlChanged.connect(self.update_url)
+        tk.Frame.__init__(self, root)
+        self.root = root
 
-        self.url_input = QLineEdit()
-        self.url_input.setPlaceholderText('Enter URL and press Enter')
-        self.url_input.returnPressed.connect(self.load_url)
+        self.init_browser()
+        self.bind("<FocusIn>", self.on_focus_in)
+        self.bind("<FocusOut>", self.on_focus_out)
 
-        layout.addWidget(self.url_input)
-        layout.addWidget(self.browser)
+    def init_browser(self):
+        window_info = cef.WindowInfo()
+        window_info.SetAsChild(self.winfo_id())
 
-        self.setLayout(layout)
+        self.browser = cef.CreateBrowserSync(window_info=window_info, url="https://www.google.com/")
+        self.browser.SetClientHandler(LoadHandler(self))
 
-        if url:
-            self.browser.setUrl(url)
-        else:
-            self.browser.setUrl(QUrl("https://www.google.com"))
+    def on_focus_in(self, _):
+        pass
 
-    def load_url(self):
-        url = QUrl.fromUserInput(self.url_input.text())
-        self.browser.setUrl(url)
+    def on_focus_out(self, _):
+        pass
 
-    def update_url(self, url):
-        self.url_input.setText(url.toString())
+class LoadHandler(object):
+    def __init__(self, main_frame):
+        self.main_frame = main_frame
 
-class SimpleWebBrowser(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def main():
+    sys.excepthook = cef.ExceptHook
+    cef.Initialize()
 
-        layout = QVBoxLayout()
-        main_widget = QWidget()
-        main_widget.setLayout(layout)
-        self.setCentralWidget(main_widget)
+    root = tk.Tk()
+    root.geometry("900x600")
 
-        self.tabs = QTabWidget()
-        self.tabs.setTabsClosable(True)
-        self.tabs.tabCloseRequested.connect(self.close_tab)
-        self.add_new_tab(QUrl("https://www.google.com"))
+    main_frame = MainFrame(root)
+    main_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.addTabButton = QPushButton("+")
-        self.addTabButton.clicked.connect(self.add_new_tab)
-        self.tabs.setCornerWidget(self.addTabButton)
+    def on_back_click():
+        main_frame.browser.GoBack()
 
-        layout.addWidget(self.tabs)
+    def on_forward_click():
+        main_frame.browser.GoForward()
 
-        # Add keyboard shortcuts
-        self.new_tab_shortcut = QShortcut(QKeySequence("Ctrl+T"), self)
-        self.new_tab_shortcut.activated.connect(self.add_new_tab)
-        self.close_tab_shortcut = QShortcut(QKeySequence("Ctrl+W"), self)
-        self.close_tab_shortcut.activated.connect(self.close_current_tab)
-        self.full_screen_shortcut = QShortcut(QKeySequence("F11"), self)
-        self.full_screen_shortcut.activated.connect(self.toggle_full_screen)
+    def on_reload_click():
+        main_frame.browser.Reload()
 
-    def add_new_tab(self, url=None):
-        new_tab = BrowserTab(url=url)
-        index = self.tabs.addTab(new_tab, "New Tab")
-        self.tabs.setCurrentIndex(index)
+    def on_home_click():
+        main_frame.browser.LoadUrl("https://www.google.com/")
 
-    def close_tab(self, index):
-        tab_to_close = self.tabs.widget(index)
-        tab_to_close.deleteLater()
-        self.tabs.removeTab(index)
+    back_button = tk.Button(root, text="Back", command=on_back_click)
+    back_button.pack(side=tk.LEFT)
 
-    def close_current_tab(self):
-        current_index = self.tabs.currentIndex()
-        self.close_tab(current_index)
+    forward_button = tk.Button(root, text="Forward", command=on_forward_click)
+    forward_button.pack(side=tk.LEFT)
 
-    def toggle_full_screen(self):
-        if self.isFullScreen():
-            self.showNormal()
-        else:
-            self.showFullScreen()
+    reload_button = tk.Button(root, text="Reload", command=on_reload_click)
+    reload_button.pack(side=tk.LEFT)
 
-global app
+    home_button = tk.Button(root, text="Home", command=on_home_click)
+    home_button.pack(side=tk.LEFT)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    browser = SimpleWebBrowser()
-    browser.show()
-    sys.exit(app.exec_())
+    root.mainloop()
+    cef.Shutdown()
+
+if __name__ == "__main__":
+    main()
